210085aa983cc34b8c075bf93a2bcac9b7adbefe
Miguel Corvo Diaz <mikelangelon@gmail.com>
2022-01-09 10:49:24 +0100

    Render one embedded subImage

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bytes"
+	_ "embed"
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"image"
+	_ "image/png"
+	"log"
 )
+
+//go:embed pixel_style1.png
+var b []byte
+
+var coins *ebiten.Image
+var buttons *ebiten.Image
+
+func init() {
+	img, _, err := image.Decode(bytes.NewReader(b))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buttons, err = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Game struct{}
 
@@ -12,7 +34,10 @@ func (g *Game) Update(screen *ebiten.Image) error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hellow, World!")
+	// Render the sub-image. Only the red part should be rendered.
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(200, 100)
+	screen.DrawImage(buttons.SubImage(image.Rect(16*5, 0, 16*6, 16)).(*ebiten.Image), op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -21,7 +46,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hellow World!")
+	ebiten.SetWindowTitle("Hello World!")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		panic(err)
 	}
